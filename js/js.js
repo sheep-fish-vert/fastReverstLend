2a35cb31375dd56ed58eca5dde1ee7ce6c82eaa0
Dmitry <dmitry1939@gmail.com>
2016-07-15 17:15:06 +0300

    added slick & reversted sliders

--- js/js.js
+++ js/js.js
@@ -13,6 +13,48 @@ $(document).ready(function() {
 
     });
     */
+
+    $('.block_2_2').each(function(index){
+
+        $(this).find('.slide_big').addClass('slide_main_'+index);
+        $(this).find('.slide_small').addClass('slide_submain_'+index);
+        var mainClass = '.slide_main_'+index;
+        var submainClass = '.slide_submain_'+index;
+
+        $(this).find('.slide_main_'+index).slick({
+            slidesToShow:1,
+            slidesToShow:1,
+            arrows:false,
+            fade:true,
+            asNavFor:submainClass,
+            draggable:false
+        });
+
+        $(this).find('.slide_submain_'+index).slick({
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            asNavFor: mainClass,
+            dots: false,
+            arrows: true,
+            focusOnSelect: true,
+            responsive: [
+                {
+                    breakpoint: 768,
+                    settings: {
+                        slidesToShow:2
+                    }
+                },
+                {
+                    breakpoint:480,
+                    settings:{
+                        slidesToShow:1
+                    }
+                }
+            ]
+        });
+
+    });
+
     function modalHide(){
         $('.menu .btn-menu').click(function(event) {
             //event.preventDefault();
